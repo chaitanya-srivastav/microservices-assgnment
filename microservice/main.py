21c8ece90c86672bac387d6385fbebb7f831f765
Chaitanya Srivastav <chaitanyas@shopalyst.com>
2017-12-06 20:52:32 +0530

    microservice commit 15

--- microservice/main.py
+++ microservice/main.py
@@ -69,13 +69,13 @@ def nextFlight():
                     type: string
                     default: 111
     """
-    # microuser = os.environ["MICROUSERNAME"]
-    # micropass = os.environ["MICROPASSWORD"]
-    r = requests.get('https://us-central1-airasiawebanalytics.cloudfunctions.net/interviewAPIdata/nextflight', auth=('airasia', 'AllStars9'))
-    # all_flights_info = r.json()
-    # dsc = request.args.get('DepartureStationCode')
-    # filter_based_on_input = [{"2": flight_info["NEXT_ARRIVALSTATION"] + dsc, "source_id": flight_info["customerID"]} for flight_info in all_flights_info if flight_info["NEXT_DEPARTURESTATION"] == dsc]
-    # resp = {"key_id": dsc, "contacts": filter_based_on_input}
+    microuser = os.environ["MICROUSERNAME"]
+    micropass = os.environ["MICROPASSWORD"]
+    r = requests.get('https://us-central1-airasiawebanalytics.cloudfunctions.net/interviewAPIdata/nextflight', auth=(microuser, micropass))
+    all_flights_info = r.json()
+    dsc = request.args.get('DepartureStationCode')
+    filter_based_on_input = [{"2": flight_info["NEXT_ARRIVALSTATION"] + dsc, "source_id": flight_info["customerID"]} for flight_info in all_flights_info if flight_info["NEXT_DEPARTURESTATION"] == dsc]
+    resp = {"key_id": dsc, "contacts": filter_based_on_input}
     return jsonify(r)
 
 @auth.verify_password
