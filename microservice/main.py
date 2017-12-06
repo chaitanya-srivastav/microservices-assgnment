04cebb228b33c2d5e2e83313e1f4cfda2cbdaf9c
Chaitanya Srivastav <chaitanyas@shopalyst.com>
2017-12-06 21:08:08 +0530

    microservice commit 16

--- microservice/main.py
+++ microservice/main.py
@@ -17,6 +17,7 @@ from flask import make_response
 from flasgger import Swagger
 import requests
 import os
+import urllib2, base64
 from flask_httpauth import HTTPBasicAuth
 
 app = Flask(__name__)
@@ -71,12 +72,15 @@ def nextFlight():
     """
     microuser = os.environ["MICROUSERNAME"]
     micropass = os.environ["MICROPASSWORD"]
-    r = requests.get('https://us-central1-airasiawebanalytics.cloudfunctions.net/interviewAPIdata/nextflight', auth=(microuser, micropass))
-    all_flights_info = r.json()
+    request = urllib2.Request("https://us-central1-airasiawebanalytics.cloudfunctions.net/interviewAPIdata/nextflight")
+    base64string = base64.b64encode('%s:%s' % (microuser, micropass))
+    request.add_header("Authorization", "Basic %s" % base64string)
+    result = urllib2.urlopen(request)
+    # all_flights_info = result
     dsc = request.args.get('DepartureStationCode')
-    filter_based_on_input = [{"2": flight_info["NEXT_ARRIVALSTATION"] + dsc, "source_id": flight_info["customerID"]} for flight_info in all_flights_info if flight_info["NEXT_DEPARTURESTATION"] == dsc]
-    resp = {"key_id": dsc, "contacts": filter_based_on_input}
-    return jsonify(r)
+    # filter_based_on_input = [{"2": flight_info["NEXT_ARRIVALSTATION"] + dsc, "source_id": flight_info["customerID"]} for flight_info in all_flights_info if flight_info["NEXT_DEPARTURESTATION"] == dsc]
+    # resp = {"key_id": dsc, "contacts": filter_based_on_input}
+    return jsonify(result)
 
 @auth.verify_password
 def verify_password(username, password):
